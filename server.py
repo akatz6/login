90acd524bca57809123bb45b075cd44b828db30f
aaronmk2 <aaronmk2@yahoo.com>
2016-06-10 14:04:49 -0700

    login complete

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
+from flask.ext.bcrypt import Bcrypt
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 mysql = MySQLConnector(app,'login')
+bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -12,45 +14,29 @@ def new_user():
 	query = """INSERT INTO login (first_name, last_name, email, 
 		password, created_at, updated_at)VALUES
 		(:first_name, :last_name, :email, :password, NOW(), NOW())"""
+	password = request.form['password']
+
 	data = {
 	'first_name': request.form['first_name'], 
 	'last_name':  request.form['last_name'],
 	'email': request.form['email'],
-	'password': request.form['password']
+	'password': bcrypt.generate_password_hash(password)
 	}
-	print date
 	mysql.query_db(query, data)
 	return render_template('index.html')
 
-# @app.route('/friends/<id>/edit', methods=['POST'])
-# def edit(id):
-
-# 	query = "SELECT * FROM friends where id = :id"
-# 	data = {
-# 	'id':id
-# 	}
-# 	result = mysql.query_db(query, data)
-# 	print result
-# 	return render_template('update.html', friend = result)
-
-
-# @app.route('/friends/<id>/test', methods=['POST'])
-# def update(id):
-# 	query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
-# 	data = {
-# 	'first_name': request.form['first_name'],
-# 	'last_name':  request.form['last_name'],
-# 	'occupation': request.form['occupation'],
-# 	'id': id
-# 	}
-# 	mysql.query_db(query, data)
-# 	return redirect('/')
-
-# @app.route('/friends/<id>/delete', methods=['POST'])
-# def destroy(id):
-# 	query = "DELETE FROM friends WHERE id = :id"
-# 	data = {'id': id}
-# 	mysql.query_db(query, data)
-# 	return redirect('/')
+@app.route('/login', methods=['POST'])
+def login():
+	email = request.form['email']
+	password = request.form['password']
+	user_query = "SELECT * FROM login WHERE email = :email LIMIT 1"
+	query_data = { 'email': email }
+	user = mysql.query_db(user_query, query_data) 
+	print user
+	if bcrypt.check_password_hash(user[0]['password'], password):
+		return render_template('login.html')
+	else:
+		flash("password does not match")
+	return render_template('index.html')
 
 app.run(debug=True)
